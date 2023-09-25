90acb834eddfc3a22caa1f8e253673a4226c1f7b
yashG0 <yashgaurkar0@gmail.com>
2023-09-25 17:16:12 +0530

    new music app

--- app/page.js
+++ app/page.js
@@ -1,113 +1,114 @@
-import Image from 'next/image'
+'use client'
+import Image from 'next/image';
+import { useState } from 'react';
+import { FaPlay, FaPause, FaForward, FaBackward } from 'react-icons/fa';
+import AudioPlayer from 'react-audio-player';
+
 
 export default function Home() {
+  const [music, setMusic] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const toggleMusic = () => {
+    setIsPlaying(!isPlaying);
+    setMusic(!music)
+    const audioElement = document.querySelector("audio");
+    if (isPlaying) {
+      audioElement.pause();
+    } else {
+      setTimeout(() => {
+        audioElement.play();
+      }, 100); // Add a short delay (e.g., 100 milliseconds) before playing
+    }
+  };
+
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+
+  const nextSong = () => {
+    const nextIndex = (currentSongIndex + 1) % songs.length;
+    setCurrentSongIndex(nextIndex);
+    setMusic(true);
+    setIsPlaying(true);
+  };
+
+  const prevSong = () => {
+    const prevIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+    setCurrentSongIndex(prevIndex);
+    setMusic(true);
+    setIsPlaying(true);
+  };
+
+  const songs = [
+    {
+      name: 'Faded',
+      singer: 'Alan Walker',
+      link: '/faded.mp3',
+      songImg: '/faded_img.jpg', // Add the background image path for this song
+      bgColor:'#b0c7eb'
+    },
+    {
+      name: 'Hurts So Good',
+      singer: 'Astrid S',
+      link: '/hurts.mp3',
+      songImg: '/hurts_img.jpg', // Add the background image path for this song
+      bgColor:'#f7d6cd'
+    },
+    {
+      name: 'Sigma Male',
+      singer: 'Zedline',
+      link: '/sigma.mp3',
+      songImg: '/sigma_img.jpg', // Add the background image path for this song
+      bgColor:'#bbabc4'
+    },
+    {
+      name: 'Bella Ciao',
+      singer: 'Hardwell, Maddix',
+      link: '/bella.mp3',
+      songImg: '/bella_img.jpg', // Add the background image path for this song
+      bgColor:'#e0949d'
+    },
+  ];
+
+
+  const currentSong = songs[currentSongIndex];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className='min-h-screen w-full bg-gray-300 flex justify-center items-center'>
+        {/* MUSIC CONTAINER */}
+        <div style={{backgroundColor:currentSong.bgColor}}  className={`absolute md:w-[20%] h-[70%] w-[80%] flex items-center md:h-3/4 shadow-xl shadow-gray-400 rounded-xl`} >
+          {/* INNER CONTAINER */}
+          <div className='p-4 text-center m-auto flex-col h-[80%] flex justify-between w-fit'>
+            <div className='text-xl md:text-2xl text-gray-900'>{currentSong.name}</div>
+            <div className='text-base md:text-lg text-gray-600 -mt-12 capitalize'>{currentSong.singer}</div>
+            
+            <div
+              className={`w-[195px] h-[195px] md:w-[230px] md:h-[230px] rounded-full bgImg shadow-lg shadow-gray-800 ${!music ? 'img_stop_rotate' : 'img_rotate'}`}
+              style={{ backgroundImage: `url(${currentSong.songImg})` }}
+            ></div>
+
+            <div className='flex justify-between'>
+              <div className='hover:cursor-pointer active:bg-gray-800 active:text-white p-[6px] rounded-full' onClick={prevSong}>
+                <FaBackward />
+              </div>
+              <div className='hover:cursor-pointer  active:bg-gray-800 active:text-white p-[6px] rounded-full' onClick={toggleMusic}>
+                {music ? <FaPause /> : <FaPlay />}
+              </div>
+              <div className='hover:cursor-pointer active:bg-gray-800 active:text-white p-[6px] rounded-full' onClick={nextSong}>
+                <FaForward />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      {/* Audio Player (Place it outside the div with play/pause button) */}
+      <AudioPlayer
+        src={currentSong.link}
+        autoPlay={isPlaying}
+        controls={false} // Hide default controls
+      />
+    </>
+  );
 }
